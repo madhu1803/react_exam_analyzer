f7c994f75edd7193b506595440c8bb2618eddfba
madhumithaa <madhumithaa@madhumithaas-MacBook-Air.local>
2020-10-10 11:09:08 +0530

    jumbo removed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
 import { Jumbotron } from "react-bootstrap";
+import { Input } from "../components/index";
+
 export default class Login extends Component {
   render() {
     return (
-      <div className="login-container row">
-        <div className="left-login-container col-lg-6">
-          <Jumbotron className="login-jumbotron">
-            <h1>Hello, world!</h1>
-            <p>
-              This is a simple hero unit, a simple jumbotron-style component for
-              calling extra attention to featured content or information.
-            </p>
-          </Jumbotron>
+      <div className="login-container">
+        <div className="box-container">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="login-left-container">hello</div>
+            </div>
+            <div className="col-lg-6">
+              <div className="login-right-container">
+                <p>Login</p>
+                <Input />
+                <Input />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="right-login-container col-lg-6">hello</div>
       </div>
     );
   }
