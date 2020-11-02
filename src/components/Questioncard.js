cfbc7961d4179a03200f10b0e2e83a20b79c8eaa
madhumithaa <madhumithaa@madhumithaas-MacBook-Air.local>
2020-11-02 19:20:05 +0530

    done

--- src/components/Questioncard.js
+++ src/components/Questioncard.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
 import { Card, Button } from "react-bootstrap";
+
 export default class Questioncard extends Component {
   render() {
+    let { name, description, question_paper } = this.props;
+
     return (
       <Card className="shadow-lg p-3 mb-5 bg-white rounded mt-5">
-        <Card.Header>Featured</Card.Header>
+        <Card.Header>{name}</Card.Header>
         <Card.Body>
-          <Card.Title>Special title treatment</Card.Title>
-          <Card.Text>
-            With supporting text below as a natural lead-in to additional
-            content.
-          </Card.Text>
+          <Card.Title>{description}</Card.Title>
+          <Card.Text>{question_paper}</Card.Text>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
--- src/pages/QuestionPaperView.js
+++ src/pages/QuestionPaperView.js
@@ -1,16 +1,40 @@
 import React, { Component } from "react";
 import { Navibar, Questioncard } from "../components/index";
+import axios from "axios";
 export default class QuestionPaperView extends Component {
+  state = {
+    qpaper: [],
+  };
+  componentDidMount() {
+    axios({
+      method: "get",
+      url:
+        "https://testing.ajaidanial.wtf/examination/question-papers/?show-questions=true",
+      headers: {
+        Authorization: `Token ${localStorage.getItem("auth_key")}`,
+      },
+    })
+      .then((response) => {
+        this.setState({
+          ...this.state,
+          qpaper: response.data,
+        });
+        console.log(response);
+
+        console.log(this.state.qpaper);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  }
   render() {
     return (
       <div>
         <Navibar />
         <div className="container">
-          <Questioncard />
-          <Questioncard />
-          <Questioncard />
-          <Questioncard />
-          <Questioncard />
+          {this.state.qpaper.map((qpaper) => (
+            <Questioncard {...qpaper} />
+          ))}
         </div>
       </div>
     );
