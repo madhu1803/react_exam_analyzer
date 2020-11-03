8fcf86bd7a1ff0f888bfd91ca9307443b7be0310
ajaidanial <ajai@geopits.com>
2020-11-03 10:14:49 +0530

    reinit project and recleaned

--- src/helpers/Routes.js
+++ src/helpers/Routes.js
@@ -1,35 +1,35 @@
-import { Redirect, Route } from "react-router-dom";
-import React from "react";
+import { Redirect, Route } from 'react-router-dom'
+import React from 'react'
 
 // protected route will not allow the user to access the page unless hes logged in
 export const AuthenticatedRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
     render={(props) =>
-      localStorage.getItem("auth_key") ? (
+      localStorage.getItem('auth_key') ? (
         <Component {...props} />
       ) : (
         <Redirect
           to={{
-            pathname: "/login",
-            state: { from: props.location },
+            pathname: '/login',
+            state: { from: props.location }
           }}
         />
       )
     }
   />
-);
+)
 
 // non protected route like a login page will only be allowed to access if the user is logged out
 export const NonAuthenticatedRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
     render={(props) =>
-      localStorage.getItem("auth_key") ? (
+      localStorage.getItem('auth_key') ? (
         <Redirect
           to={{
-            pathname: "/home",
-            state: { from: props.location },
+            pathname: '/home',
+            state: { from: props.location }
           }}
         />
       ) : (
@@ -37,4 +37,4 @@ export const NonAuthenticatedRoute = ({ component: Component, ...rest }) => (
       )
     }
   />
-);
+)
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,7 @@
-import React, { Component } from "react";
-import { Navibar, HomeCard } from "../components/index";
+import React, { Component } from 'react'
+
 export default class Home extends Component {
   render() {
-    return (
-      <div className="home-container">
-        <Navibar />
-        <div className="container pt-5">
-          <div className="row">
-            <div className="col-lg-4">
-              <HomeCard />
-            </div>
-            <div className="col-lg-4">
-              <HomeCard />
-            </div>
-            <div className="col-lg-4">
-              <HomeCard />
-            </div>
-            <div className="col-lg-6">
-              <HomeCard />
-            </div>
-            <div className="col-lg-6">
-              <HomeCard />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+    return <div>Home</div>
   }
 }
